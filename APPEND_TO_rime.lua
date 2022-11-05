3be5a6dbcb545a3a8910f7a9266bd97819d0002d
geezmolycos <djy0212@foxmail.com>
2022-11-05 17:04:20 +0800

    更改了目录结构，使方案更加便携

--- APPEND_TO_rime.lua
+++ APPEND_TO_rime.lua
@@ -1,5 +1,13 @@
 
+-- Begin flutterpy loader
+
+local user_data_dir = rime_api:get_user_data_dir()
+package.path = package.path .. ';' .. user_data_dir .. '/flutterpy_data/?.lua'
+package.path = package.path .. ';' .. user_data_dir .. '/flutterpy_data/?/init.lua'
+
 -- 加载flutterpy的对象和函数
 for k, v in pairs(require('flutterpy')) do
     _G['flutterpy_' .. k] = v
 end
+
+-- End flutterpy loader
